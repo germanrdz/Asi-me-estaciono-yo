45c46b3efec8b833f93576571bbf76a1b4b0ec26
GerManson <german@phisoft.com.mx>
2011-07-12 20:48:48 -0700

    ignoring facebook configuration file

--- application/config/facebook.php
+++
@@ -1,4 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-$config["facebook_appid"] = '133512650062074';
-$config["facebook_secret"] = '98dc67b994a453e68788a501c36612be';
